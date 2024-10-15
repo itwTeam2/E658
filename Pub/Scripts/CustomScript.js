558e11253adaef2759b69d7cf9df973bb492e293
itwTeam2 <itwteam2@gmail.com>
2024-10-15 09:47:17 +0530

    did change in not loading things when published the project (This did in master branch)

---
+++ Pub/Scripts/CustomScript.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+    // Select all <td> elements and log their text content
+    $('#example td').each(function () {
+        console.log($(this).text());
+    });
+
+
+    ///Created BY   : Sqn Ldr Wicky
+    ///Created Date : 2024/07/22
+    ///Description : load E658 details in to a Model Pop up
+
+    /// Drop down list in Operation Contribution
+    $(function () {
+        $('#btnViewInfo').click(function () {
+
+            var E658CreatorID = $("#hndECDID").val();
+            var EFlowID = $("#hndEFTID").val();
+            var RoleID = $("#hndRoleID").val();
+
+            var route = '@Url.Action("E658DetailsFinalView", "E658")?ECDID=' + E658CreatorID + '&EFlowID=' + EFlowID + '&RoleId=' + RoleID;
+
+            console.log(route);
+
+            //$.ajax({
+            //    url: route,
+            //    type: 'GET',
+            //    success: function (data) {
+            //        console.log('Success:', data);
+            //    },
+            //    error: function (xhr, status, error) {
+            //        console.log('Error:', error);
+            //    }
+            //});
+
+            //$('#partial2').load(route);
+        });
+    });
